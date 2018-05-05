461abeda2d1e3e623a3404e2c81653e0a911e677
ravikanthtadikonda1979 <ravikanth.x.tadikonda@outlook.com>
2018-05-05 21:03:19 +0530

    Added new terraform file to create ec2 and s3 bucket

---
+++ launch_instance.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+	access_key=""
+	secret_key=""
+	region="eu-west-1"
+}
+
+resource "aws_instance" "sampleec2" {
+	ami = "ami-9cbe9be5"
+	instance_type ="t2.micro"
+	security_groups=["ravikanth_security_group"]
+	tags{
+		Name = "terraform-instance"
+	}
+}
+
+resource "aws_s3_bucket" "ravikanths3bucket" {
+	bucket = "ravikanths3bucket"
+	acl="private"
+	tags{
+		name="My Bucket"
+		Environment = "Dev"
+	}
+	versioning {
+		enabled = true
+	}
+}
